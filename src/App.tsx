82526562b5c512023c681e07c71407efbb35fc7f
alimoosavi <alimusavi67@gmail.com>
2022-05-24 08:40:36 +0430

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import GlobalStyle from './styles';
 
 function App() {
   return (
+    <>
+    <GlobalStyle />
     <div className="App">
       
     </div>
+    </>
   );
 }
 
